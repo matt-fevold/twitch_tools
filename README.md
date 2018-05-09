b8c77901ad181a3bebc66b7458645fb013daedb2
Matthew Fevold <matt.fevold@gmail.com>
2018-05-08 23:08:29 -0500

    update readme, clean code up

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# 
+# Twitch-Tools
+
+A simple program to display the current playing spotify song.
+
+Motivation - I did not want to run a large application just to run the current playing song (snip) and I wanted a bit more control over how that's displayed.
+
+### to run:
+
+download requirements.
+
+Run main. Follow prompt.
+
+
+
+### how it works:
+
+It finds the window id for spotify (why you have to click on it, as finding the foreground window was dead simple)
+ and prints off the name of that window (for spotify it conviniently is ```Song - Artist```) 
+ 
+: )
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import win32gui
 import time
 
+
 def main():
     print("Finding Spotify Window. Please click open Spotify.")
-    time.sleep(3)
-    window = win32gui.get_foreground_window()
+    time.sleep(10)
+    window = win32gui.GetForegroundWindow()
     current_song = win32gui.GetWindowText(window)
-    # print("WindowID: '{}'".format(window))
     print("Currently Playing:", current_song)
     while True:
         # ugly but works for now.
@@ -16,7 +16,7 @@ def main():
         else:
             current_song = maybe_new_song
             print("\nCurrently Playing:", current_song)
-# 197282
+
 
 if __name__ == '__main__':
     main()
